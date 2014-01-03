25afebb754d4bddda0a9547ec9c4b5e84036316d
Robpol86 <robpol86@gmail.com>
2014-01-03 15:09:19 -0800

    Finished migrating all tests to py.test.
    Migrated test_logging to py.test from unittest. All tests pass.
    
    PyCharm added __author__ to all my __init__.py files. Made them empty
    like I want.

---
+++ tests/test_logging/test_logging.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python2.6
+import libs
+import logging
+import logging.config
+import logging.handlers
+import sys
+import time
+
+
+def log_samples():
+    """Writes sample log messages to the root logger. Returns the expected timestamp."""
+    logging.debug("Test debug testing.")
+    logging.info("Test info testing.")
+    logging.warn("Test warn testing.")
+    logging.error("Test error testing.")
+    logging.critical("Test critical testing.")
+    return time.strftime("%Y-%m-%dT%H:%M:%S")
+
+
+def log_samples_named():
+    """Same as log_samples() except instead of 'root' it will use its own name in log statements."""
+    logger = logging.getLogger('test_logging')
+    logger.debug("Test debug testing.")
+    logger.info("Test info testing.")
+    logger.warn("Test warn testing.")
+    logger.error("Test error testing.")
+    logger.critical("Test critical testing.")
+    return time.strftime("%Y-%m-%dT%H:%M:%S")
+
+
+def test_default(capsys):
+    config = libs.get_config(dict(), test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert 1 == len(logging.getLogger().handlers)
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.ConsoleHandler)
+
+    log_samples()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = "Test info testing.\n"
+    stderr_expected = "Test warn testing.\nTest error testing.\nTest critical testing.\n"
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+
+def test_quiet(capsys):
+    config = libs.get_config({'--quiet': True}, test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert len(logging.getLogger().handlers) == 1
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.NullHandler)
+
+    log_samples()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = ""
+    stderr_expected = ""
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+
+def test_logfile(capsys, log_file):
+    config = libs.get_config({'--log': log_file.name}, test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert 2 == len(logging.getLogger().handlers)
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.ConsoleHandler)
+    assert isinstance(logging.getLogger().handlers[1], libs.LoggingSetup.TimedRotatingFileHandler)
+
+    timestamp = log_samples()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = "Test info testing.\n"
+    stderr_expected = "Test warn testing.\nTest error testing.\nTest critical testing.\n"
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+    log_actual = log_file.read(1024)
+    log_expected = "%s INFO     root                           Test info testing.\n" % timestamp
+    log_expected += "%s WARNING  root                           Test warn testing.\n" % timestamp
+    log_expected += "%s ERROR    root                           Test error testing.\n" % timestamp
+    log_expected += "%s CRITICAL root                           Test critical testing.\n" % timestamp
+    assert log_expected == log_actual
+
+
+def test_verbose(capsys):
+    config = libs.get_config({'--verbose': True}, test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert len(logging.getLogger().handlers) == 1
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.ConsoleHandler)
+
+    log_samples()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = "Test debug testing.\nTest info testing.\n"
+    stderr_expected = "Test warn testing.\nTest error testing.\nTest critical testing.\n"
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+
+def test_quiet_logfile(capsys, log_file):
+    config = libs.get_config({'--quiet': True, '--log': log_file.name}, test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert len(logging.getLogger().handlers) == 1
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.TimedRotatingFileHandler)
+
+    timestamp = log_samples()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = ""
+    stderr_expected = ""
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+    log_actual = log_file.read(1024)
+    log_expected = "%s INFO     root                           Test info testing.\n" % timestamp
+    log_expected += "%s WARNING  root                           Test warn testing.\n" % timestamp
+    log_expected += "%s ERROR    root                           Test error testing.\n" % timestamp
+    log_expected += "%s CRITICAL root                           Test critical testing.\n" % timestamp
+    assert log_expected == log_actual
+
+
+def test_quiet_verbose(capsys):
+    config = libs.get_config({'--quiet': True, '--verbose': True}, test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert len(logging.getLogger().handlers) == 1
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.NullHandler)
+
+    log_samples()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = ""
+    stderr_expected = ""
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+
+def test_logfile_verbose(capsys, log_file):
+    config = libs.get_config({'--log': log_file.name, '--verbose': True}, test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert 2 == len(logging.getLogger().handlers)
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.ConsoleHandler)
+    assert isinstance(logging.getLogger().handlers[1], libs.LoggingSetup.TimedRotatingFileHandler)
+
+    timestamp = log_samples()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = "Test debug testing.\nTest info testing.\n"
+    stderr_expected = "Test warn testing.\nTest error testing.\nTest critical testing.\n"
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+    log_actual = log_file.read(1024)
+    log_expected = "%s DEBUG    root                           Test debug testing.\n" % timestamp
+    log_expected += "%s INFO     root                           Test info testing.\n" % timestamp
+    log_expected += "%s WARNING  root                           Test warn testing.\n" % timestamp
+    log_expected += "%s ERROR    root                           Test error testing.\n" % timestamp
+    log_expected += "%s CRITICAL root                           Test critical testing.\n" % timestamp
+    assert log_expected == log_actual
+
+
+def test_quiet_logfile_verbose(capsys, log_file):
+    config = libs.get_config({'--quiet': True, '--log': log_file.name, '--verbose': True}, test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert len(logging.getLogger().handlers) == 1
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.TimedRotatingFileHandler)
+
+    timestamp = log_samples()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = ""
+    stderr_expected = ""
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+    log_actual = log_file.read(1024)
+    log_expected = "%s DEBUG    root                           Test debug testing.\n" % timestamp
+    log_expected += "%s INFO     root                           Test info testing.\n" % timestamp
+    log_expected += "%s WARNING  root                           Test warn testing.\n" % timestamp
+    log_expected += "%s ERROR    root                           Test error testing.\n" % timestamp
+    log_expected += "%s CRITICAL root                           Test critical testing.\n" % timestamp
+    assert log_expected == log_actual
+
+
+def test_logfile_multiple_loggers(capsys, log_file):
+    config = libs.get_config({'--log': log_file.name}, test=True)
+    with libs.LoggingSetup(config['verbose'], config['log'], config['quiet']) as f:
+        logging.config.fileConfig(f.config)  # Setup logging.
+    assert 2 == len(logging.getLogger().handlers)
+    assert isinstance(logging.getLogger().handlers[0], libs.LoggingSetup.ConsoleHandler)
+    assert isinstance(logging.getLogger().handlers[1], libs.LoggingSetup.TimedRotatingFileHandler)
+
+    timestamp = log_samples()
+    time.sleep(1)
+    timestamp_named = log_samples_named()
+    stdout_actual, stderr_actual = capsys.readouterr()
+    stdout_expected = "Test info testing.\nTest info testing.\n"
+    stderr_expected = "Test warn testing.\nTest error testing.\nTest critical testing.\n"
+    stderr_expected += "Test warn testing.\nTest error testing.\nTest critical testing.\n"
+    assert stdout_expected == stdout_actual
+    assert stderr_expected == stderr_actual
+
+    log_actual = log_file.read(1024)
+    log_expected = "%s INFO     root                           Test info testing.\n" % timestamp
+    log_expected += "%s WARNING  root                           Test warn testing.\n" % timestamp
+    log_expected += "%s ERROR    root                           Test error testing.\n" % timestamp
+    log_expected += "%s CRITICAL root                           Test critical testing.\n" % timestamp
+    log_expected += "%s INFO     test_logging                   Test info testing.\n" % timestamp_named
+    log_expected += "%s WARNING  test_logging                   Test warn testing.\n" % timestamp_named
+    log_expected += "%s ERROR    test_logging                   Test error testing.\n" % timestamp_named
+    log_expected += "%s CRITICAL test_logging                   Test critical testing.\n" % timestamp_named
+    assert log_expected == log_actual
